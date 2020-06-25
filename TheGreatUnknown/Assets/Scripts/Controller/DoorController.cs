9ae728d7dd28c88151fb2c34efcf583f9b86ecde
Michael H <63379713+MichaelHalbl@users.noreply.github.com>
2020-06-25 18:49:30 +0200

    Added DoorControll, BoxMesh, WaldMesh
    Added DoorControll, BoxMesh, WaldMesh

---
+++ TheGreatUnknown/Assets/Scripts/Controller/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool doorIsOpening;
+    void Update()
+    {
+         // Wenn der boolean wert true liefert öffnet sich die Tür
+        if (doorIsOpening == true)
+        {           
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        // Wenn der wert größer 7f ist wird die tür gestoppt
+        if(Door.transform.position.y > 7f) {
+            doorIsOpening = false;
+        }
+    }
+    //Wenn man mit der maus klickt öffnet sich die Tür
+    void OnMouseDown() {
+        doorIsOpening = true;
+    }
+}
